8d994dbfb1e6b38e519fb08fbb5f99a292df7a2e
Eяw!и Gontier <ierwinx@me.com>
2022-07-31 15:45:15 -0500

    Initial Commit

---
+++ Formularios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Formularios
+//
+//  Created by Erwin Luz León on 31/07/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Formularios/FormulariosApp.swift
@@ -0,0 +1,17 @@
+//
+//  FormulariosApp.swift
+//  Formularios
+//
+//  Created by Erwin Luz León on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct FormulariosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
